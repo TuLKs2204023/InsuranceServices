00f1e1fffed7aa9c95ac5e0a991870f21af43980
levankydu <levankydu94@gmail.com>
2023-10-10 21:17:47 +0700

    kydu  final comit

---
+++ Models/Home_Insurance.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test0000001.Models
+{
+    public class Home_Insurance
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Gmail { get; set; }
+        public string? YearBuilt { get; set; }
+        public string? Structure { get; set; }
+        public string? NumberOfBasement { get; set; }
+        public int Area { get; set; }
+        public string? LinkDriver { get; set; }
+        public string? Status { get; set; } = "Not approved yet";
+        //[ForeignKey(nameof(Policy))]
+        //public int PolicyId { get; set; } // Foreign Key
+        public virtual Policy? Policy { get; set; }
+        [ForeignKey(nameof(Duration))]
+        public int DurationId { get; set; } // Foreign Key
+        public virtual Duration? Duration { get; set; } 
+        //public ICollection<Photos>? Photos { get; set; }
+
+
+    }
+}
