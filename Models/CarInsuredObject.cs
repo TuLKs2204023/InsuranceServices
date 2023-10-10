dc62857a4f1711de48bc26d36423ca3fc4545c69
Tu <khuta441@gmail.com>
2023-10-10 21:15:36 +0700

    v2.0.0.0

---
+++ Models/CarInsuredObject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace test0000001.Models
+{
+    [Table(name: ("tbCarInsuredObject"))]
+    public class CarInsuredObject
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime YearsOfManufacture { get; set; }
+        
+        [StringLength(50)]
+        public string? Automaker { get; set; }
+        [StringLength(50)]
+        public string? CarBand { get; set; }
+        [StringLength(50)]
+        public string? CarType { get; set; }
+        [StringLength(50)]
+        public string? CityOfCarReg { get; set; }
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+
+    }
+}
